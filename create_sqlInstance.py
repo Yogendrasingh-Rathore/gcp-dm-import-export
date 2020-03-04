ac76e48e639940b712318f7e90944518b855bffc
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2020-03-04 15:14:01 +0530

    Update create_sqlInstance.py

--- create_sqlInstance.py
+++ create_sqlInstance.py
@@ -1,21 +1,36 @@
 import sys
 import os
 
-def create_sqlInstance():
+def create_dm():
   try:
       deployment_name = raw_input('Enter Deployment Name: ')
       template_fileName = raw_input('Enter Template File Name: ')
       template_filePath = os.getcwd() + '/Tmpl/' + template_fileName + '.tmpl'
       config_fileName = template_fileName + '.yaml'
-      #config_filePath = os.getcwd() + '/Yaml/' + config_fileName
       os.system('envsubst < {0} > {1}'.format(template_filePath, config_fileName))
       deployment_manager_cmd = 'gcloud deployment-manager deployments \
-                                create {0} --config {1} '.format(
+                                create {0} --config {1} --automatic-rollback-on-error'.format(
                                 deployment_name, config_fileName)
       os.system(deployment_manager_cmd)
   except Exception as e:
       print(str(e))
-      print("Exception in creating SQL INSTANCE")
+      print("Exception in creating Deployment Manager")
 
 
-create_sqlInstance()
+def enable_gcloud_apis():
+    try:
+        gcloud_service_list = {'deploymentmanager.googleapis.com',
+                                'sqladmin.googleapis.com',
+                                'storage-api.googleapis.com',
+                              }
+        for each in gcloud_service_list:
+            cmd = 'gcloud services enable {0}'.format(each)
+            print(cmd)
+            os.system(cmd)
+    except Exception as e:
+        print("Exception in enabling service")
+    return True
+
+
+enable_gcloud_apis()
+create_dm()
